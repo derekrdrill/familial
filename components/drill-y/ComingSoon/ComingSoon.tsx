362e87bda172b46ede1c50a88fda5b4fad9b3c84
Derek Drill <62970257+derekrdrill@users.noreply.github.com>
2023-11-19 10:29:57 -0500

    ✨ Creating initial Coming Soon pg (#2)
    * ✨ Creating initial Coming Soon pg
    
    * ✨ Accesibility updates to heading

--- components/drill-y/ComingSoon/ComingSoon.tsx
+++ components/drill-y/ComingSoon/ComingSoon.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Grid, Typography } from '@mui/material';
 
 const ComingSoon = () => (
-  <Grid container>
-    <Grid item xs={12}>
-      <Typography variant='subtitle1'>Coming Soon</Typography>
+  <Grid container justifyContent='center'>
+    <Typography sx={{ typography: { xs: 'h3', md: 'h1' } }} textAlign='center' variant='h1'>
+      ⚠️ Nothing to see here.... yet ⚠️
+    </Typography>
+    <Grid container justifyContent='center'>
+      <ComingSoonImage src='/bitmojiPainting.png' />
     </Grid>
+    <Grid container justifyContent='center'>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h3' } }} variant='h2'>
+        Rest assured...
+      </Typography>
+    </Grid>
+    <ComingSoonTextContainer container justifyContent='center'>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' } }} variant='h3'>
+        Some very important people are working hard to deploy this site 🚀
+      </Typography>
+    </ComingSoonTextContainer>
   </Grid>
 );
 
 export default ComingSoon;
+
+export const ComingSoonImage = styled.img({
+  height: 400,
+  width: 400,
+});
+
+export const ComingSoonTextContainer = styled(Grid)({
+  padding: 24,
+});
