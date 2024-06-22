81de4ac425700dac0c8e88bfd32e76e0d0da2e41
Derek Drill <derekrdrill@gmail.com>
2024-06-22 11:36:02 -0400

    🛠️ User profile refactors

---
+++ components/drill-y/UserProfile/UserProfile.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { SignOutButton } from '@clerk/nextjs';
+import tw from 'twin.macro';
+import styled from '@emotion/styled';
+import { Button, Grid, Typography } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+import GlobalContext from '../../../context/GlobalContext';
+
+import { DrillyTypography } from '../../../styles/globals';
+
+type UserProfileProps = {
+  isUserSidebarOpen: boolean;
+  setIsUserSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const UserProfile = ({ isUserSidebarOpen, setIsUserSidebarOpen }: UserProfileProps) => {
+  const {
+    state: { isDarkMode, user },
+  } = React.useContext(GlobalContext);
+
+  return (
+    <>
+      <Grid container>
+        <Button
+          color='secondary'
+          onClick={() => setIsUserSidebarOpen(!isUserSidebarOpen)}
+          size='small'
+          tw='m-1'
+          variant={isDarkMode ? 'outlined' : 'text'}
+        >
+          <Typography variant='h5'>&#10539;</Typography>
+        </Button>
+      </Grid>
+      <DrillyTypography
+        variant='h4'
+        textAlign='center'
+        $isDarkMode={isDarkMode}
+      >{`${user?.firstName} ${user?.lastName}`}</DrillyTypography>
+      <DrillyTypography variant='h6' textAlign='center' $isDarkMode={isDarkMode}>
+        {user?.userName}
+      </DrillyTypography>
+      <Grid container justifyContent='center' tw='mt-14'>
+        <UserProfileAvatar sx={{ fontSize: '200px' }} $isDarkMode={isDarkMode} />
+      </Grid>
+      <Grid container justifyContent='center' tw='absolute bottom-5'>
+        <SignOutButton tw='w-full bg-gray-300 opacity-60 hover:opacity-100 rounded-lg m-3 p-2' />
+      </Grid>
+    </>
+  );
+};
+
+export default UserProfile;
+
+export const UserProfileAvatar = styled(AccountCircleIcon)<{ $isDarkMode?: boolean }>(
+  ({ $isDarkMode }) => [$isDarkMode && tw`text-gray-300`, !$isDarkMode && tw`text-gray-600`],
+);
