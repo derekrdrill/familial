0bff11b8d4b9a9a40332eadae3ec60b7ea7cf710
Derek Drill <derekrdrill@gmail.com>
2024-09-09 00:38:42 -0400

    deletes album cover url if photo is deleted

--- pages/api/photo/delete.ts
+++ pages/api/photo/delete.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { NextApiResponse, NextApiRequest } from 'next';
 
 import conn from '../../../data/connection';
-import { Photos } from '../../../data/models';
+import { Albums, Photos } from '../../../data/models';
 
 export default async function DELETE(req: NextApiRequest, res: NextApiResponse) {
   await conn();
@@ -10,6 +10,16 @@ export default async function DELETE(req: NextApiRequest, res: NextApiResponse) 
   const albumName = JSON.parse(req.body).albumName;
   const photoId = JSON.parse(req.body).photoId;
 
+  const currentAlbum = await Albums.findOne({ albumName });
+  const currentPhoto = await Photos.findOne({ _id: photoId });
+
+  if (currentAlbum.albumCoverURL === currentPhoto.url) {
+    await Albums.updateOne(
+      { _id: new mongoose.Types.ObjectId(currentAlbum._id) },
+      { $set: { albumCoverURL: null } },
+    );
+  }
+
   await Photos.deleteOne({ _id: new mongoose.Types.ObjectId(photoId) });
-  res.json(await Photos.find({ albumName: albumName }).sort({ uploadedAt: -1 }));
+  res.json(await Photos.find({ albumName }).sort({ uploadedAt: -1 }));
 }
