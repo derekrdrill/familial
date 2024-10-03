0e4d27b794e226d1aa96563262eb25f868b82762
Derek Drill <derekrdrill@gmail.com>
2024-10-03 10:13:08 -0400

    RecipeRandomLoading

--- components/familial/Recipes/components/RecipeRandom/RecipeRandom.tsx
+++ components/familial/Recipes/components/RecipeRandom/RecipeRandom.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import styled from '@emotion/styled';
 import tw from 'twin.macro';
-import { CircularProgress } from '@mui/material';
 import CachedIcon from '@mui/icons-material/Cached';
 import LaunchIcon from '@mui/icons-material/Launch';
 
@@ -11,6 +10,7 @@ import GlobalContext from '../../../../../context/GlobalContext';
 import RecipeRandomMetadata from './components/RecipeRandomMetadata';
 import { DrillyTypography } from '../../../../../styles/globals';
 import { GlobalReducerActionEnum } from '../../../../../context/GlobalReducer';
+import RecipeRandomShimmer from './components/RecipeRandomShimmer';
 
 export default function RecipeRandom() {
   const router = useRouter();
@@ -23,63 +23,67 @@ export default function RecipeRandom() {
 
   return (
     <div tw='gap-2 grid grid-cols-1 mt-12 md:grid-cols-2 md:gap-24'>
-      <Image
-        height={0}
-        alt='recipe-image'
-        sizes='100vw'
-        src={recipeRandom?.imageUrl ?? '/dinner2.webp'}
-        tw='max-h-80 object-cover rounded-3xl w-full'
-        width={0}
-      />
-      <div tw='flex flex-col gap-3'>
-        <div>
-          <DrillyTypography component='h2' tw='font-main text-4xl' $isDarkMode={isDarkMode}>
-            {recipeRandom?.title}
-          </DrillyTypography>
-          <DrillyTypography component='p' tw='font-main text-xl' $isDarkMode={isDarkMode}>
-            by {recipeRandom?.author}
-          </DrillyTypography>
-        </div>
-        <div tw='flex flex-col gap-1'>
-          <RecipeRandomMetadata recipeRandomKey='type' />
-          <RecipeRandomMetadata recipeRandomKey='temperature' />
-          <RecipeRandomMetadata recipeRandomKey='time' />
-          <RecipeRandomMetadata recipeRandomKey='ingredients' />
-          <RecipeRandomMetadata recipeRandomKey='steps' />
-        </div>
-        <div tw='flex gap-2'>
-          <RecipeRandomButton
-            onClick={async () => {
-              setIsRandomRecipeLoading(true);
+      {isRandomRecipeLoading && <RecipeRandomShimmer />}
+      {!isRandomRecipeLoading && (
+        <>
+          <Image
+            height={0}
+            alt='recipe-image'
+            sizes='100vw'
+            src={recipeRandom?.imageUrl ?? '/dinner2.webp'}
+            tw='max-h-80 object-cover rounded-3xl w-full'
+            width={0}
+          />
+          <div tw='flex flex-col gap-3'>
+            <div>
+              <DrillyTypography component='h2' tw='font-main text-4xl' $isDarkMode={isDarkMode}>
+                {recipeRandom?.title}
+              </DrillyTypography>
+              <DrillyTypography component='p' tw='font-main text-xl' $isDarkMode={isDarkMode}>
+                by {recipeRandom?.author}
+              </DrillyTypography>
+            </div>
+            <div tw='flex flex-col gap-1'>
+              <RecipeRandomMetadata recipeRandomKey='type' />
+              <RecipeRandomMetadata recipeRandomKey='temperature' />
+              <RecipeRandomMetadata recipeRandomKey='time' />
+              <RecipeRandomMetadata recipeRandomKey='ingredients' />
+              <RecipeRandomMetadata recipeRandomKey='steps' />
+            </div>
+            <div tw='flex gap-2'>
+              <RecipeRandomButton
+                onClick={async () => {
+                  setIsRandomRecipeLoading(true);
 
-              await fetch(`/api/recipe/get?isRandom=true`).then(async res => {
-                const recipeRandomResponse = await res.json();
-                const recipeRandom = recipeRandomResponse[0];
+                  await fetch(`/api/recipe/get?isRandom=true`).then(async res => {
+                    const recipeRandomResponse = await res.json();
+                    const recipeRandom = recipeRandomResponse[0];
 
-                dispatch({
-                  type: GlobalReducerActionEnum.SET_RECIPE_RANDOM,
-                  payload: { recipeRandom: recipeRandom },
-                });
-              });
+                    dispatch({
+                      type: GlobalReducerActionEnum.SET_RECIPE_RANDOM,
+                      payload: { recipeRandom: recipeRandom },
+                    });
+                  });
 
-              setIsRandomRecipeLoading(false);
-            }}
-          >
-            Show another{' '}
-            {isRandomRecipeLoading ? <CircularProgress size='small' /> : <CachedIcon />}
-          </RecipeRandomButton>
-          <RecipeRandomButton
-            onClick={() => {
-              router.push({
-                pathname: `/recipes/${recipeRandom?._id}`,
-              });
-            }}
-            $isSecondary
-          >
-            Go to recipe <LaunchIcon />
-          </RecipeRandomButton>
-        </div>
-      </div>
+                  setIsRandomRecipeLoading(false);
+                }}
+              >
+                Show another <CachedIcon />
+              </RecipeRandomButton>
+              <RecipeRandomButton
+                onClick={() => {
+                  router.push({
+                    pathname: `/recipes/${recipeRandom?._id}`,
+                  });
+                }}
+                $isSecondary
+              >
+                Go to recipe <LaunchIcon />
+              </RecipeRandomButton>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
